1e328780481dd27d45f6d99abba43a3f711a51eb
ssse0y <ko527ko@gmail.com>
2022-09-14 00:32:39 +0900

    edit

--- main.c
+++ main.c
@@ -4,6 +4,10 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
+	int a, b;
 	printf("Hello, Sookmyung Electronics!\n");
+	a=10;
+	b=20;
+	printf("%d+%d = %d\n",a,b,a+b);
 	return 0;
 }
